9fca94ceff573b1e830290381e7024ac9e486235
Sarai <saraiflores@gmail.com>
2019-02-13 15:01:39 -0500

    following quick js tutorial

---
+++ codecampReact.js
@@ -0,0 +1,29 @@
+class Hello extends React.Component{
+constructor(){
+  super();
+  this.state= {
+    message: "my friend from (state)!"
+  };
+  this.updateMessage= this.updateMessage.bind(this);
+}
+
+updateMessage(){
+  this.setState({
+    message: 'my friend (from changed state)!'
+  });
+}
+
+render() {
+  return (
+    <div>
+      <h1>Hello {this.props.message}!</h1>
+      <button onClick={this.updateMessage}>Click me!</button>
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(
+    <Hello message='my friend'/>,
+    document.getElementById("root")
+);
